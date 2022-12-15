72017aba83fc5b6a079d56b23275239287996111
luan.mai-mesoneer <35148022+maituongluanvn@users.noreply.github.com>
2022-12-15 22:14:33 +0700

    restructure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import {Header} from "@components/header/Header"
+import Head from 'next/head';
+import Image from 'next/image';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import Header from '@components/Header/Header';
+// import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <Header/>
-    </div>
-  )
+    <>
+      <Header />
+      <Grid
+        item
+        xs={12}
+        md={8}
+        sx={{
+          '& .markdown': {
+            py: 3,
+          },
+        }}
+      >
+        <Typography variant="h6" gutterBottom>
+          This is title
+        </Typography>
+        <Divider />
+        content
+      </Grid>
+    </>
+  );
 }
