f1a20eb63fc05463f4dab9de95f8bd9602d6676a
luan.mai-mesoneer <35148022+maituongluanvn@users.noreply.github.com>
2022-12-15 21:23:16 +0700

    #2 still init project

---
+++ components/Header/HeaderLinks.tsx
@@ -0,0 +1,121 @@
+/*eslint-disable*/
+import React from 'react';
+import Link from 'next/link';
+
+// @material-ui/core components
+import { makeStyles } from '@mui/styles';
+import { List, ListItem, Tooltip, Icon } from '@mui/material';
+
+// @material-ui/icons
+import { Apps, CloudDownload, AutoDelete, SmartButton } from '@mui/icons-material';
+// core components
+import CustomDropdown from '../CustomDropdown/CustomDropdown';
+import Button from '../CustomButtons/Button';
+
+import styles from '../../styles/jss/nextjs-material-kit/components/headerLinksStyle';
+
+const useStyles = makeStyles(styles);
+
+export default function HeaderLinks(props) {
+  const classes = useStyles();
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          navDropdown
+          buttonText="Components"
+          buttonProps={{
+            className: classes.navLink,
+            color: 'transparent',
+          }}
+          buttonIcon={Apps}
+          dropdownList={[
+            <Link href="/components">
+              <a className={classes.dropdownLink}>All components</a>
+            </Link>,
+            <a
+              href="https://creativetimofficial.github.io/nextjs-material-kit/#/documentation?ref=njsmk-navbar"
+              target="_blank"
+              className={classes.dropdownLink}
+            >
+              Documentation
+            </a>,
+          ]}
+        />
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button href="blog" color="transparent" className={classes.navLink}>
+          <i className={classes.socialIcons + ' fa fa-rss'} />
+          Blog
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="https://github.com/app-generator/nextjs-material-kit"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <CloudDownload className={classes.icons} /> Download
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        {/*<Tooltip title="Delete">
+          <IconButton aria-label="Delete">
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>*/}
+        <Tooltip
+          id="instagram-twitter"
+          title="Follow us on twitter"
+          placement={'top'}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            href="https://twitter.com/CreativeTim?ref=creativetim"
+            target="_blank"
+            color="transparent"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + ' fab fa-twitter'} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-facebook"
+          title="Follow us on facebook"
+          placement={'top'}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.facebook.com/CreativeTim?ref=creativetim"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + ' fab fa-facebook'} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-tooltip"
+          title="Follow us on instagram"
+          placement={'top'}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + ' fab fa-instagram'} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+    </List>
+  );
+}
